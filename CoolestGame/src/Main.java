9a5921e24d703251a52fb71d2effaa7955212a32
mikehopps <mhopps@gmail.com>
2017-03-13 07:47:57 -0400

    Created Main.java

---
+++ CoolestGame/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by michael_hopps on 3/13/17.
+ */
+public class Main {
+
+    //Hi Casey!
+    public static void main(String[] args) {
+        System.out.println("This is a test.");
+        int x = 4;
+    }
+}

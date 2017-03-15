560635a77d619f535b70d29c48e79bf632039c6a
cassandra_varnau <cassandra_varnau@student.wayland.k12.ma.us>
2017-03-15 10:08:04 -0400

    made 2 extensions of the Sprite class

--- CoolestGame/src/FoodCat.java
+++ CoolestGame/src/FoodCat.java
@@ -1,8 +1,18 @@
+import java.awt.*;
+
 /**
  * Created by cassandra_varnau on 3/15/17.
  */
 public class FoodCat extends Sprite {
 
+    public FoodCat(World aworld) {
+        super(aworld);
+        setPic("", NORTH);
+        int a = (int)((Math.random() * 1200));
+        int b = (int)((Math.random() * 800 ));
+        setLoc(new Point(a,b));
+        setSpeed(1);
+    }
     // we will eat these to get bigger
     // they will randomly spawn
     // we will need more and more each time to level up

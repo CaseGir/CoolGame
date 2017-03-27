068dae91c5725f093fe4ad7f5983086332e5bbf7
crystal_wong <crystal_wong@student.wayland.k12.ma.us>
2017-03-27 10:08:31 -0400

    added hit box to yum yum food

--- CoolestGame/src/FoodCat.java
+++ CoolestGame/src/FoodCat.java
@@ -1,12 +1,22 @@
 import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * Created by cassandra_varnau on 3/15/17.
  */
 public class FoodCat extends Sprite {
 
-    public FoodCat(World aworld) {
-        super(aworld);
+    private Rectangle leftSide, rightSide, top, bottom;
+    private int width, height;
+
+    public FoodCat(int x, int y, int w, int h) {
+        super(x,y);
+        width = w;
+        height = h;
+        leftSide = new Rectangle(-5, -5, 10, height+10);
+        rightSide = new Rectangle(width-5, -5, 10, height+10);
+        top = new Rectangle(-5, -5, width+10, 10);
+        bottom = new Rectangle(-5, height-5, width+10, 10);
         setPic("", NORTH);
         int a = (int)((Math.random() * 1200));
         int b = (int)((Math.random() * 800 ));
@@ -16,7 +26,34 @@ public class FoodCat extends Sprite {
 
     @Override
     public void update(){
+        public boolean isOnScreen(x,y){
+            return (x > -1 && x < width && y > -1 && y < height);
+        }
 
+        public boolean hitLeftSide(Sprite spr){
+            Rectangle spriteBox = spr.getBoundingRectangle();
+            if(spriteBox.intersects(leftSide))
+                return true;
+            return false;
+        }
+        public boolean hitRightSide(Sprite spr){
+            Rectangle spriteBox = spr.getBoundingRectangle();
+            if(spriteBox.intersects(rightSide))
+                return true;
+            return false;
+        }
+        public boolean hitTop(Sprite spr){
+            Rectangle spriteBox = spr.getBoundingRectangle();
+            if(spriteBox.intersects(top))
+                return true;
+            return false;
+        }
+        public boolean hitBottom(Sprite spr){
+            Rectangle spriteBox = spr.getBoundingRectangle();
+            if(spriteBox.intersects(bottom))
+                return true;
+            return false;
+        }
     }
 
     // we will eat these to get bigger

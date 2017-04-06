af27d1bb455a38504a11244f86503b5b0a9561cd
cassandra_varnau <cassandra_varnau@student.wayland.k12.ma.us>
2017-04-06 10:58:10 -0400

    fixed the main

--- CoolestGame/src/GodCat.java
+++ CoolestGame/src/GodCat.java
@@ -6,18 +6,12 @@ import java.awt.*;
 public class GodCat extends Sprite {
     //better food
 
-    public GodCat(int x, int y, int dir){
-        super(x,y, EAST);
-        setPic("GodCat.png", NORTH);
-        int a = (int)((Math.random() * 1200));
-        int b = (int)((Math.random() * 800 ));
-        setLoc(new Point(a,b));
+    public GodCat(int x, int y, int dir) {
+        super(x, y, EAST);
+        setPic("GodCat.png", EAST);
+        int a = (int) ((Math.random() * 1200));
+        int b = (int) ((Math.random() * 800));
+        setLoc(new Point(a, b));
         setSpeed(1);
     }
-
-    @Override
-    public void update(){
-      super.update();
-    }
-
 }
